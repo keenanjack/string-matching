99a1f174188ce1eb17a1aeb03e4f7db2038451f6
Jack Keenan <jack@Jacks-MBP.lan1>
2019-09-22 18:13:01 -0400

    finalized code

--- stringmatch.py
+++ stringmatch.py
@@ -1,13 +1,82 @@
 import sys
 import re
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from scipy.sparse import csr_matrix
+import sparse_dot_topn.sparse_dot_topn as ct
 
-strings_to_match = 
-lookup_strings = 
+#Strings to lookup, must be in csv format
+all_strings = sys.argv[1]
+all_strings = pd.read_csv(all_strings).iloc[:,0]
 
 def ngrams(string, n=3):
     string = re.sub(r'[,-./]|\sBD',r'', string)
     ngrams = zip(*[string[i:] for i in range(n)])
     return [''.join(ngram) for ngram in ngrams]
 
-print(ngrams('McDonalds'))
+def generate_matrix(all_strings):
+    vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams)
+    tf_idf_matrix = vectorizer.fit_transform(all_strings)
+    return tf_idf_matrix
+
+def awesome_cossim_top(A, B, ntop, lower_bound=0):
+    # force A and B as a CSR matrix.
+    # If they have already been CSR, there is no overhead
+    A = A.tocsr()
+    B = B.tocsr()
+    M, _ = A.shape
+    _, N = B.shape
+ 
+    idx_dtype = np.int32
+ 
+    nnz_max = M*ntop
+ 
+    indptr = np.zeros(M+1, dtype=idx_dtype)
+    indices = np.zeros(nnz_max, dtype=idx_dtype)
+    data = np.zeros(nnz_max, dtype=A.dtype)
+
+    ct.sparse_dot_topn(
+        M, N, np.asarray(A.indptr, dtype=idx_dtype),
+        np.asarray(A.indices, dtype=idx_dtype),
+        A.data,
+        np.asarray(B.indptr, dtype=idx_dtype),
+        np.asarray(B.indices, dtype=idx_dtype),
+        B.data,
+        ntop,
+        lower_bound,
+        indptr, indices, data)
+
+    return csr_matrix((data,indices,indptr),shape=(M,N))
+
+def get_matches_df(sparse_matrix, name_vector,top=None):
+    non_zeros = sparse_matrix.nonzero()
+    
+    sparserows = non_zeros[0]
+    sparsecols = non_zeros[1]
+    
+    if top:
+        nr_matches = top
+    else:
+        nr_matches = sparsecols.size
+    
+    left_side = np.empty([nr_matches], dtype=object)
+    right_side = np.empty([nr_matches], dtype=object)
+    similairity = np.zeros(nr_matches)
+    
+    for index in range(0, nr_matches):
+        left_side[index] = name_vector[sparserows[index]]
+        right_side[index] = name_vector[sparsecols[index]]
+        similairity[index] = sparse_matrix.data[index]
+    
+    return pd.DataFrame({'left_side': left_side,
+                          'right_side': right_side,
+                           'similairity': similairity})
+        
+
+matrix_match = generate_matrix(all_strings)
+
+matches = awesome_cossim_top(matrix_match,matrix_match.transpose(),10,0.5)
+matches_df = get_matches_df(matches,all_strings)
+
+print(matches_df)
